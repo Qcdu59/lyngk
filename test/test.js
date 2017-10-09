5a320b22322567f47cb8a2d6258029698a01bb29
qcoloos <quentin.coloos@gmail.com>
2017-10-09 11:09:49 +0200

    Sixieme histoire, test ecrit

--- test/test.js
+++ test/test.js
@@ -35,5 +35,11 @@ LyngkTestCase.prototype.testD = function(){
 LyngkTestCase.prototype.testE = function(){
     var c = new Lyngk.Coordinates('A',3);
     var clone = c.clone();
-    assertTrue(c.toString() === clone.toString())
+    assertTrue(c.toString() === clone.toString());
+}
+
+LyngkTestCase.prototype.testF = function(){
+    var c = new Lyngk.Coordinates('B',4);
+    var hash = c.hash();
+    assertTrue(c.hash === 24);
 }
